b19a486efe2520a52fd1a52e79506b02e5778d16
Albert Bregonia <47435340+albertbregonia@users.noreply.github.com>
2026-06-26 22:14:47 -0400

    feat: add FinalJeopardy struct and top level JeopardyConfig
    this marks completion of the object shapes for the Jeopardy game

--- jeopardy-rs/src/game/jeopardy/config.rs
+++ jeopardy-rs/src/game/jeopardy/config.rs
@@ -1,5 +1,131 @@
-// `JeopardyConfig` is the top level struct encapsulating
-// the entirety of the Jeopardy game configuration (board, final jeopardy, etc)
-// not including state (points, daily doubles, etc.)
-// - main purpose is to make this reusable across instances and allow for user-defined
-// custom boards with custom questions, etc.
+use serde::{Deserialize, Serialize};
+
+use crate::game::jeopardy::{
+    JeopardyBoardError, board::Board, final_jeopardy::FinalJeopardy, non_empty_vec,
+};
+
+/// `JeopardyConfig` is the top level struct encapsulating
+/// the entirety of the Jeopardy game configuration (board, final jeopardy, etc)
+/// not including state (points, daily doubles, etc.)
+/// The main purpose is to make this reusable across game lobby instances
+/// and allow for user-defined custom boards with custom questions, etc.
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct JeopardyConfig {
+    #[serde(deserialize_with = "non_empty_vec")]
+    boards: Vec<Board>,
+    final_jeopardy: FinalJeopardy,
+}
+
+impl JeopardyConfig {
+    pub fn new(
+        boards: Vec<Board>,
+        final_jeopardy: FinalJeopardy,
+    ) -> Result<Self, JeopardyBoardError> {
+        if boards.is_empty() {
+            return Err(JeopardyBoardError::EmptyBoardList);
+        }
+        Ok(Self {
+            boards,
+            final_jeopardy,
+        })
+    }
+
+    pub fn boards(&self) -> &[Board] {
+        &self.boards
+    }
+
+    pub fn boards_mut(&mut self) -> &mut [Board] {
+        &mut self.boards
+    }
+
+    pub fn final_jeopardy(&self) -> &FinalJeopardy {
+        &self.final_jeopardy
+    }
+}
+
+#[cfg(test)]
+use crate::server::TestDefault;
+
+#[cfg(test)]
+impl TestDefault for JeopardyConfig {
+    fn test_default() -> Self {
+        // GIVEN
+        let boards = vec![Board::test_default()];
+        let final_jeopardy = FinalJeopardy::test_default();
+
+        // WHEN
+        let mut config = Self::new(boards.clone(), final_jeopardy.clone()).unwrap();
+
+        // THEN
+        assert_eq!(&boards, config.boards());
+        assert_eq!(&boards, config.boards_mut());
+        assert_eq!(&final_jeopardy, config.final_jeopardy());
+        config.boards_mut()[0].assign_daily_double(1).unwrap(); // won't compile if not mut
+
+        config
+    }
+}
+
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod jeopardy_config_tests {
+    use serde_json::json;
+
+    use crate::game::jeopardy::board::Board;
+    use crate::game::jeopardy::{
+        JeopardyBoardError, config::JeopardyConfig, final_jeopardy::FinalJeopardy,
+    };
+    use crate::server::TestDefault;
+
+    #[test]
+    fn GIVEN_jeopardy_config_WHEN_new_THEN_ok() {
+        JeopardyConfig::test_default();
+    }
+
+    #[test]
+    fn GIVEN_jeopardy_config_WHEN_deserialize_THEN_ok() {
+        // GIVEN
+        let boards = vec![Board::test_default()];
+        let final_jeopardy = FinalJeopardy::test_default();
+        let json = json!({
+            "boards": boards.clone(),
+            "final_jeopardy": final_jeopardy.clone(),
+        });
+
+        // WHEN
+        let mut config = serde_json::from_value::<JeopardyConfig>(json).unwrap();
+
+        // THEN
+        assert_eq!(&boards, config.boards());
+        assert_eq!(&boards, config.boards_mut());
+        assert_eq!(&final_jeopardy, config.final_jeopardy());
+        config.boards_mut()[0].assign_daily_double(1).unwrap(); // won't compile if not mut
+    }
+
+    #[test]
+    fn GIVEN_empty_board_list_WHEN_new_THEN_error() {
+        // GIVEN
+        let boards = vec![];
+
+        // WHEN
+        let result = JeopardyConfig::new(boards, FinalJeopardy::test_default());
+
+        // THEN
+        assert!(matches!(result, Err(JeopardyBoardError::EmptyBoardList)))
+    }
+
+    #[test]
+    fn GIVEN_empty_board_list_WHEN_deserialize_THEN_error() {
+        // GIVEN
+        let json = json!({
+            "boards": [],
+            "final_jeopardy": FinalJeopardy::test_default(),
+        });
+
+        // WHEN
+        let result = serde_json::from_value::<JeopardyConfig>(json);
+
+        // THEN
+        assert!(matches!(result, Err(serde_json::Error { .. })))
+    }
+}
--- jeopardy-rs/src/game/jeopardy/final_jeopardy.rs
+++ jeopardy-rs/src/game/jeopardy/final_jeopardy.rs
@@ -1,1 +1,107 @@
+use serde::{Deserialize, Serialize};
+
+use crate::game::jeopardy::question::Question;
+use crate::game::jeopardy::{JeopardyBoardError, non_empty_trimmed};
+
 // defines Final Jeopardy as a `Question` with a hint string
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+pub struct FinalJeopardy {
+    #[serde(deserialize_with = "non_empty_trimmed")]
+    hint: String,
+    question: Question,
+}
+
+impl FinalJeopardy {
+    pub fn new(hint: String, question: Question) -> Result<Self, JeopardyBoardError> {
+        let hint = hint.trim();
+        if hint.is_empty() {
+            return Err(JeopardyBoardError::EmptyHint);
+        }
+        Ok(Self {
+            hint: hint.to_owned(),
+            question,
+        })
+    }
+
+    pub fn hint(&self) -> &str {
+        &self.hint
+    }
+
+    pub fn question(&self) -> &Question {
+        &self.question
+    }
+}
+
+#[cfg(test)]
+use crate::server::TestDefault;
+
+#[cfg(test)]
+impl TestDefault for FinalJeopardy {
+    fn test_default() -> Self {
+        let hint = "hint".to_string();
+        let question = Question::test_default();
+
+        // WHEN
+        let final_jeopardy = Self::new(hint.clone(), question.clone()).unwrap();
+
+        // THEN
+        assert_eq!(hint, final_jeopardy.hint());
+        assert_eq!(&question, final_jeopardy.question());
+
+        final_jeopardy
+    }
+}
+
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod final_jeopardy_tests {
+    use serde_json::json;
+
+    use crate::game::jeopardy::{
+        JeopardyBoardError, final_jeopardy::FinalJeopardy, question::Question,
+    };
+    use crate::server::TestDefault;
+
+    #[test]
+    fn GIVEN_final_jeopardy_WHEN_new_THEN_ok() {
+        FinalJeopardy::test_default();
+    }
+
+    #[test]
+    fn GIVEN_empty_hint_WHEN_new_THEN_error() {
+        // GIVEN
+        let question = Question::test_default();
+        let hint = "".to_string();
+
+        // WHEN - truly empty
+        let result = FinalJeopardy::new(hint, question.clone());
+
+        // THEN
+        assert!(matches!(result, Err(JeopardyBoardError::EmptyHint)));
+
+        // GIVEN
+        let hint = "\t".to_string();
+        // WHEN - whitespace only
+        let result = FinalJeopardy::new(hint, question.clone());
+
+        // THEN
+        assert!(matches!(result, Err(JeopardyBoardError::EmptyHint)));
+    }
+
+    #[test]
+    fn GIVEN_empty_hint_WHEN_deserialize_THEN_error() {
+        // GIVEN
+        let question = Question::test_default();
+        let json = json!({
+            "hint": "", // empty hint
+            "question": question,
+        });
+
+        // WHEN
+        let result = serde_json::from_value::<FinalJeopardy>(json);
+
+        // THEN
+        assert!(matches!(result, Err(serde_json::Error { .. })));
+    }
+}
