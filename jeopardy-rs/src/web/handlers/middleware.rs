ce3c8d1433a3ea44e0c26b61c20f15971e10d95d
Albert Bregonia <47435340+albertbregonia@users.noreply.github.com>
2026-06-26 20:01:37 -0400

    test: add unit test for request ID middleware
    uses a tower oneshot to hook into the standard axum ecosystem

--- jeopardy-rs/src/web/handlers/middleware.rs
+++ jeopardy-rs/src/web/handlers/middleware.rs
@@ -20,3 +20,50 @@ pub async fn request_id_middleware(
     req.extensions_mut().insert(request_id);
     next.run(req).instrument(span).await
 }
+
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod middleware_tests {
+    use std::sync::Arc;
+    use tower::util::ServiceExt;
+
+    use axum::{Router, body::Body, extract::State, http::Request, middleware, routing::get};
+    use tokio::sync::mpsc;
+    use uuid::Uuid;
+
+    use crate::web::handlers::middleware::request_id_middleware;
+
+    #[tokio::test]
+    async fn GIVEN_middlware_WHEN_request_THEN_ok() {
+        // create a channel that we can listen to if the server panics
+        let (tx, mut rx) = mpsc::channel(1);
+
+        // sample handler to ensure that our middleware has inserted the request id
+        let test_handler = move |State(tx): State<Arc<mpsc::Sender<_>>>, req: Request<Body>| async move {
+            let result = tokio::spawn(async move {
+                let request_id = req
+                    .extensions()
+                    .get::<Uuid>();
+                    assert!(matches!(
+                        request_id, // ensure the request extensions have a uuid / request id
+                        Some(Uuid{..})
+                    ));
+            }).await;
+            tx.send(result).await.unwrap();
+        };
+
+        let request = Request::builder().uri("/").body(Body::empty()).unwrap();
+
+        // run oneshot web server
+        tokio::spawn(
+            Router::new()
+            .route("/", get(test_handler))
+            .layer(middleware::from_fn(request_id_middleware))
+            .with_state(Arc::new(tx))
+            .oneshot(request)
+        );
+        // if the server panics, we panic too (test fails)
+        // otherwise, test passes and we unwrap a unit
+        rx.recv().await.unwrap().unwrap();
+    }
+}
