1170d2fbcfae98d65435d3744358a1514a2be5a2
Albert Bregonia <47435340+albertbregonia@users.noreply.github.com>
2026-07-25 22:53:04 -0400

    feat: add Cell react component and styles
    base component for a Jeopardy question on a game board

--- static/frontend/src/App.tsx
+++ static/frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import { Cell } from "./components/cell/Cell"
+
 function App() {
-	return (
-		<></>
-	)
+    return (
+        <>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <Cell>Sample Cell 1</Cell>
+                <Cell>Sample Cell 2</Cell>
+                <Cell>Sample Cell 3</Cell>
+                <Cell>Sample Cell 4</Cell>
+            </div>
+        </>
+    )
 }
 
 export default App
--- static/frontend/src/components/cell/Cell.tsx
+++ static/frontend/src/components/cell/Cell.tsx
@@ -1,5 +1,18 @@
-function Cell() {
+import type { MouseEventHandler, ReactNode } from 'react'
+import './Cell.css'
+
+export interface CellProps {
+    children?: ReactNode
+    className?: string
+    onClick?: MouseEventHandler<HTMLDivElement>
+}
+
+// Cells are a simple bordered <div> that takes the size of the available space.
+// It is designed to be used as part of a collection of <Cell>'s that will evenly distribute themselves.
+export function Cell({ children, className = "cell", onClick = () => { } }: CellProps) {
     return (
-        <></>
+        <div className={className} onClick={onClick}>
+            {children}
+        </div>
     )
 }
