1a4aa1cdc40060e106e16e0912eba6db7c63f61d
Albert Bregonia <47435340+albertbregonia@users.noreply.github.com>
2026-07-26 14:57:34 -0400

    feat: add Category component and styling for frontend
    to be used as part of a future Board componentin the included prototype styling

--- static/frontend/src/App.tsx
+++ static/frontend/src/App.tsx
@@ -1,16 +1,42 @@
-import { Cell } from "./components/cell/Cell"
+import { Category } from "./components/category/Category"
+import type { Category as JeopardyCategory } from "./types/Jeopardy";
 import './App.css'
 
 function App() {
+    // dummy data to fill the components
+    const category: JeopardyCategory = {
+        name: "Category Name",
+        questions: Array.from({ length: 6 }, () => ({
+            answered: false,
+            dailyDouble: false,
+            pointValue: Math.round(Math.random() * 1000 + 100),
+            question: {
+                content: "Question",
+                answer: "Answer"
+            }
+        }))
+    };
+    const longNamedCategory: JeopardyCategory = {
+        name: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Eos esse doloremque eum corrupti nam quibusdam perspiciatis iste veniam molestias, aut sed pariatur numquam totam, vel similique quod mollitia a.Velit!",
+        questions: Array.from({ length: 1 }, () => ({
+            answered: false,
+            dailyDouble: false,
+            pointValue: Math.round(Math.random() * 1000 + 100),
+            question: {
+                content: "Question",
+                answer: "Answer"
+            }
+        }))
+    };
     return (
-        <>
-            <div style={{ display: "flex", flexDirection: "row", color: "var(--question-points-color)" }}>
-                <Cell>Sample Cell 1</Cell>
-                <Cell>Sample Cell 2</Cell>
-                <Cell>Sample Cell 3</Cell>
-                <Cell>Sample Cell 4</Cell>
-            </div>
-        </>
+        <div className="test">
+            <Category category={longNamedCategory} />
+            <Category category={category} />
+            <Category category={category} />
+            <Category category={category} />
+            <Category category={category} />
+            <Category category={category} />
+        </div>
     )
 }
 
--- static/frontend/src/components/category/Category.tsx
+++ static/frontend/src/components/category/Category.tsx
@@ -1,6 +1,28 @@
-function Category() {
+import type { Category } from "../../types/Jeopardy"
+import { Cell } from "../cell/Cell"
+import "./Category.css"
+
+export interface CategoryProps {
+    category: Category
+}
+
+// Categories are a simple <div> that contain a collection of <Cells> 
+// that scale to the size of the available space.
+export function Category({ category }: CategoryProps) {
     return (
-        <>
-        </>
+        <div className="category">
+            <Cell className="category-name-cell">
+                <div className="category-name">{category.name}</div>
+            </Cell>
+            <div className="category-questions">{
+                category.questions
+                    .map((q, index) => // keys need to be unique
+                        <Cell key={`${category.name}-${index}`}
+                            className="category-question">
+                            <div className="points">${q.answered ? "" : q.pointValue.toString()}</div>
+                        </Cell>
+                    )
+            }</div>
+        </div>
     )
 }
