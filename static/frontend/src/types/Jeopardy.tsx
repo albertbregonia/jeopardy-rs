db11e8df98fa68ea8a18f1392481ee74a20a8ac5
Albert Bregonia <47435340+albertbregonia@users.noreply.github.com>
2026-07-25 22:41:00 -0400

    chore: add TypeScript variants of Rust constructs from Jeopardy game for interoperability between backend and frontend
    normally, we would use something like AWS Smithy to design the models and generate the types dynamically from a single source of truth. However, the constructs are so few and minimal that we can simply forego it and just manage changes in both places

---
+++ static/frontend/src/types/Jeopardy.tsx
@@ -0,0 +1,32 @@
+// derived from the rust models
+
+export interface Question {
+    content: string,
+    answer: string
+}
+
+export interface BoardQuestion {
+    pointValue: number,
+    dailyDouble: boolean,
+    answered: boolean,
+    question: Question,
+}
+
+export interface Category {
+    name: string,
+    questions: BoardQuestion[]
+}
+
+export interface Board {
+    categories: Category[]
+}
+
+export interface FinalJeopardy {
+    hint: string,
+    question: Question,
+}
+
+export interface JeopardyConfig {
+    boards: Board[],
+    final_jeopardy: FinalJeopardy,
+}
